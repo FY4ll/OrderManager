d219f70a7f63cc971e77d40a7acdf031d08b064e
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-19 19:49:03 +0200

    MainWindow.cpp Method implemented

---
+++ src/MainWindow.cpp
@@ -0,0 +1,7 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow() //Mainwindows.h method that sets the title and size of the window
+{
+    setWindowTitle("Order Manager"); //set the title of the window to "Order Manager"
+    resize(800, 600); //set the size of the window to 800x600 pixels
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,11 @@
 #include <QApplication>
-#include <QMainWindow>
-#include <QWidget>
-#include <QVBoxLayout>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QMainWindow window;
-    QWidget *centralWidget = new QWidget();
-    window.setCentralWidget(centralWidget);
-    window.setWindowTitle("Order Manager");
-    window.resize(800, 600);
-    window.show();
 
+    MainWindow mainWindow; //create an instance of the MainWindow class
+    mainWindow.show(); //show the main window
     return app.exec();
 }
