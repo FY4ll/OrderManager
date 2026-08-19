1b4a0d596bb8fb3be598d0779c0be352eaf4d0c4
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-19 19:49:03 +0200

    MainWindow.cpp Method implemented

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@ int main(int argc, char *argv[])
     QMainWindow window;
     QWidget *centralWidget = new QWidget();
     window.setCentralWidget(centralWidget);
-    window.setWindowTitle("Order Manager");
-    window.resize(800, 600);
     window.show();
 
     return app.exec();
