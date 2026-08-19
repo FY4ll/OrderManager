3000a2ea36d89fe8e7e74a41f8b2538961060e42
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-19 19:45:37 +0200

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ include/MainWindow.h
@@ -0,0 +1,8 @@
+#pragma once //the file can't be loaded twice
+#include <QMainWindow> //include the QMainWindow class from the Qt framework
+
+class MainWindow : public QMainWindow //define a new class called MainWindow that inherits from QMainWindow
+{
+    public: 
+        MainWindow();
+};
