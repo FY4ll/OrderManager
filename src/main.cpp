9de0f31b7b2a0b7c3e372908d4f461e77f583e3e
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-19 19:43:19 +0200

    Structuring main window files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QWidget>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     QMainWindow window;
+    QWidget *centralWidget = new QWidget();
+    window.setCentralWidget(centralWidget);
     window.setWindowTitle("Order Manager");
     window.resize(800, 600);
     window.show();
