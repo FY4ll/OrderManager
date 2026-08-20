2d80492896265b476ef900e96229126cb3b7d592
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-20 20:05:26 +0200

    display product on the MainWindow

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -23,5 +23,14 @@ MainWindow::MainWindow(ProductManager& productManager)//Mainwindows.h method tha
     //creating a list widget to display products
     QListWidget *productList = new QListWidget();
     layout->addWidget(productList);
+    //Giving accèss to product informations
+    const std::vector<Product>& products = productManager.getProducts();
+    //loop to get every products in the vector
+    for(std::size_t i = 0; i < products.size(); i++){
+        productList->addItem(
+            QString::number(products[i].getId()) + " | " +
+            QString::fromStdString(products[i].getName()) + " | " +
+            QString::number(products[i].getPrice()));
+    }
 
 }
