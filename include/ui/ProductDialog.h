c79c34063853807f4e5cc45051ae4e667ac630de
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-20 20:21:44 +0200

    Added productdialog class and upt cmake

---
+++ include/ui/ProductDialog.h
@@ -0,0 +1,8 @@
+#pragma once 
+#include <QDialog>
+
+class ProductDialog : public QDialog
+{
+public:
+    ProductDialog();
+};
---
+++ src/ui/ProductDialog.cpp
@@ -0,0 +1,7 @@
+#include "ui/ProductDialog.h"
+
+ProductDialog::ProductDialog()
+{
+    setWindowTitle("New Product");
+    resize(400, 300);
+}
