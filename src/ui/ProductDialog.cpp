3cf20835f9b6cdc8fe4b8be36a83231bf74d29fe
F4ll_ <95504057+FY4ll@users.noreply.github.com>
2026-08-20 20:39:30 +0200

    ProductDialog form created

--- src/ui/ProductDialog.cpp
+++ src/ui/ProductDialog.cpp
@@ -1,7 +1,27 @@
 #include "ui/ProductDialog.h"
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+#include <QPushButton>
 
 ProductDialog::ProductDialog()
 {
-    setWindowTitle("New Product");
+    //window size
+    setWindowTitle("Add New Product");
     resize(400, 300);
+    //window layout
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    QLabel *title = new QLabel("Add a new product to the list");
+    layout->addWidget(title);
+    // Name input for new product
+    QLineEdit *nameInput = new QLineEdit();
+    layout->addWidget(nameInput);
+
+    //Price input only for double value no string
+    QDoubleSpinBox *priceInput = new QDoubleSpinBox;
+    layout->addWidget(priceInput);
+
+    QPushButton *addButton = new QPushButton("Add Product to the list");
+    layout->addWidget(addButton);    
 }
